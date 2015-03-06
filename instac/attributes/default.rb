0ea5ed098afe51839473b14839640d2b9e157ca9
haladin <baihui@gbg.bg>
2015-03-06 20:22:52 +0200

    .

--- instac/attributes/default.rb
+++ instac/attributes/default.rb
@@ -1,6 +1,7 @@
 default[:mongodb] = true
 default[:document_root] = "/srv/www/autols_all"
 default[:ssl_support] = false
+default[:http_port] = 80
 default[:port] = 8080
 default[:ssl_port] = 443
 default[:nodeJS_env] = "production"
