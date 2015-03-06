8f8d69359dd9bd91a27bb028f9ac37881c8457fc
haladin <baihui@gbg.bg>
2015-03-07 00:37:49 +0200

    .

--- instac/attributes/default.rb
+++ instac/attributes/default.rb
@@ -4,4 +4,7 @@ default[:ssl_support] = false
 default[:http_port] = 80
 default[:port] = 8080
 default[:ssl_port] = 443
+default[:ssl_key_gen] = true
+default[:ssl_certificate_path] = "/etc/nginx/ssl/nginx.crt"
+default[:ssl_certificate_key_path] = "/etc/nginx/ssl/nginx.key"
 default[:nodeJS_env] = "production"
--- instac/recipes/configure_nginx.rb
+++ instac/recipes/configure_nginx.rb
@@ -5,9 +5,9 @@ template "/etc/nginx/sites-enabled/default" do
   mode 0644
 end
 
-if node[:ssl_support]
+if node[:ssl_support] and node[:ssl_key_gen]
 	Chef::Log.info("Creating new certificates....")
-	Chef::Log.info(`sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt`)	
+	Chef::Log.info(`sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout #{node[:ssl_certificate_key_path]} -out #{node[:ssl_certificate_path]}`)	
 end
 
 service "nginx" do
