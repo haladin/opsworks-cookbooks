d7e7ff978dbe93414b869bc5c0320dc6bd6873e9
haladin <baihui@gbg.bg>
2015-03-07 01:53:08 +0200

    .

--- instac/recipes/configure_nginx.rb
+++ instac/recipes/configure_nginx.rb
@@ -10,7 +10,8 @@ Chef::Log.info("Generate SSL keys Support #{node[:ssl_key_gen]}")
 
 if node[:ssl_support] and node[:ssl_key_gen]
 	Chef::Log.info("Creating new certificates....")
-	Chef::Log.info(`sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout #{node[:ssl_certificate_key_path]} -out #{node[:ssl_certificate_path]}`)	
+	Chef::Log.info(`sudo mkdir -p #{node[:ssl_certificate_path]} `)	
+	Chef::Log.info(`sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout #{node[:ssl_certificate_path]}/#{node[:ssl_certificate_key_name]} -out #{node[:ssl_certificate_path]}/#{node[:ssl_certificate_name]}`)	
 end
 
 service "nginx" do
