dd8f0f544150a04c1b9044eb1cc9332238feddca
haladin <baihui@gbg.bg>
2015-03-07 05:10:03 +0200

    .

--- instac/recipes/install_nodejs_srv_dep.rb
+++ instac/recipes/install_nodejs_srv_dep.rb
@@ -1,6 +1,6 @@
 ruby_block "Run NPM Install" do
   block do
-    Chef::Log.info("Installing node modules in #{node[:document_root]}/current NODE_ENV=#{node[:nodeJS_env
+    Chef::Log.info("Installing node modules in #{node[:document_root]}/current NODE_ENV=#{node[:nodeJS_env]}")
 	# packet.json should be fixed to include grunt not only in development
     Chef::Log.info(`sudo su - deploy -c 'cd #{node[:document_root]}/current && NODE_ENV=development npm install'`)
     raise "NPM Install FAILED" unless $?.success?
