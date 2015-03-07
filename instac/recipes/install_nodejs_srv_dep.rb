a3bcca3fa345a21087b7df48772c66a861419f94
haladin <baihui@gbg.bg>
2015-03-07 05:00:30 +0200

    .

--- instac/recipes/install_nodejs_srv_dep.rb
+++ instac/recipes/install_nodejs_srv_dep.rb
@@ -1,14 +1,15 @@
 ruby_block "Run NPM Install" do
   block do
-    Chef::Log.info("Installing node modules in #{node[:document_root]}/current NODE_ENV=#{node[:nodeJS_env]}")
-    Chef::Log.info(`sudo su - deploy -c 'cd #{node[:document_root]}/current && NODE_ENV=#{node[:nodeJS_env]} npm install'`)
+    Chef::Log.info("Installing node modules in #{node[:document_root]}/current NODE_ENV=#{node[:nodeJS_env
+	# packet.json should be fixed to include grunt not only in development
+    Chef::Log.info(`sudo su - deploy -c 'cd #{node[:document_root]}/current && NODE_ENV=development npm install'`)
     raise "NPM Install FAILED" unless $?.success?
   end
 end
 
 Chef::Log.info("Install Grunt and Boower")
 execute "install grunt" do
-  command "sudo NODE_ENV=#{node[:nodeJS_env]} npm install -g bower grunt grunt-cli"
+  command "sudo NODE_ENV=#{node[:nodeJS_env]} npm install -g bower grunt-cli"
   not_if "[ -f /usr/local/bin/grunt ]"
 end
 
