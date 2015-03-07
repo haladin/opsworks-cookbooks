c7e0bfc6d8504af7d0f1c99b508a7f0589bac6f8
haladin <baihui@gbg.bg>
2015-03-07 02:58:51 +0200

    .

--- instac/attributes/default.rb
+++ instac/attributes/default.rb
@@ -11,4 +11,5 @@ default[:ssl_key_gen] = true
 default[:ssl_certificate_path] = "/etc/nginx/ssl"
 default[:ssl_certificate_key_name] = "nginx.key"
 default[:ssl_certificate_name] = "nginx.crt"
+default[:ssl_certificate_subj] = "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com"
 default[:nodeJS_env] = "production"
